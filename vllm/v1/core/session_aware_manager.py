68bc3a66b5594eb8940e1ade32d1aefe5cd5e67e
f00806056 <fangtingzhu@huawei.com>
2026-07-21 16:21:19 +0800

    通知前检查hash是否为空

--- vllm/v1/core/session_aware_manager.py
+++ vllm/v1/core/session_aware_manager.py
@@ -178,11 +178,12 @@ class SessionAwareManager:
             )
 
         # 通知SPM保护hash
-        block_hashes = self._session_block_hash[session_id]
-        self._notify_event(
-            "session_blocks_protected",
-            block_hashes=block_hashes,
-        )
+        block_hashes = self._session_block_hash.get(session_id, [])
+        if block_hashes:
+            self._notify_event(
+                "session_blocks_protected",
+                block_hashes=block_hashes,
+            )
 
 
     def on_block_cache_hit(
